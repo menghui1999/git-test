e3d8382ce4d04bf386c64b233d08b9e71c755865
MengHui <menghui1999@foxmail.com>
2023-01-02 16:35:38 +0800

    first commit

---
+++ src/main/java/com/atgu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atgu.git;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ *
+ * @author MengHui
+ * @create 2023-01-02 16:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
